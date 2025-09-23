caac2e0915371002e993b24606c0bc03bbb0d00b
LOUQLIAIMARIAM <mariamlouqliai450@gmail.com>
2025-09-23 12:15:58 +0100

    day1

---
+++ challenges/Variables/challenge6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+  //Challenge 6 : Calcul et affichage des résultats
+
+  int a,b;
+  printf("Entrez  nombre a :");
+  scanf("%d",&a);
+  printf("Entrez  nombre b :");
+  scanf("%d",&b);
+
+  printf("a + b = %d",a+b);
+  printf("a - b = %d",a-b);
+  printf("a x b = %d",a*b);
+  printf("a / b = %d",a/b);
+
+  return 0;
+
+}
